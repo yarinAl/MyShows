395bd162eadf1a2c3fbae6d5d21081e635bee0ce
yarin <yarin@gmail.com>
2023-10-25 18:12:30 +0300

    added node server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 import express from 'express'
 
-const express = require('express')
-
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('welcome to shows server!')
+  res.send('Welcome to my server!')
 })
 
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
